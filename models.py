7c57b098dacee5391c6c96757afbcf4796c23cd5
Wyatt Roersma <wyatt.roersma@target.com>
2018-10-18 15:15:14 -0600

    Add a room table to track created rooms Update sermo plugin to add rooms to the room table Display room list in default page

--- models.py
+++ models.py
@@ -4,6 +4,20 @@ import udatetime
 from app import db
 
 
+class Rooms(db.Model):
+    """Chat Database Table."""
+
+    __tablename__ = "chat_rooms"
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(64), index=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=udatetime.utcnow)
+
+    def __repr__(self):
+        """AUCR chat plugin return messages."""
+        return '<ChatRooms {}>'.format(self.room_name)
+
+
 class Chat(db.Model):
     """Chat Database Table."""
 
@@ -12,10 +26,9 @@ class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(64), db.ForeignKey('user.username'))
     message = db.Column(db.String(512), index=True)
-    room = db.Column(db.String(16), index=True)
+    room = db.Column(db.String(64), db.ForeignKey('chat_rooms.room_name'))
     timestamp = db.Column(db.DateTime, index=True, default=udatetime.utcnow)
 
     def __repr__(self):
         """AUCR chat plugin return messages."""
         return '<Chat {}>'.format(self.message)
-
--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 """AUCR chat plugin route page handler."""
 # coding=utf-8
+from app import db
 from flask import Blueprint
-from flask import session, redirect, url_for, render_template, request
+from flask import session, redirect, url_for, render_template, request, flash
 from flask_login import current_user, login_required
 from app.plugins.Sermo.forms import ChatForm
 from app.plugins.Sermo import events
+from app.plugins.Sermo.models import Rooms
 
 
 chat_page = Blueprint('chat', __name__, template_folder='templates')
@@ -28,11 +30,21 @@ def chat_index():
         form = ChatForm(request.form)
         if form.validate_on_submit():
             room = form.room.data
+            test_for_room = Rooms.query.filter_by(room_name=room).first()
+            if not test_for_room:
+                new_room = Rooms(room_name=room, author_id=current_user.id)
+                db.session.add(new_room)
+                db.session.commit()
             session['room'] = room
             url_for_string = url_for('chat.chat_room', room=room)
             return redirect(url_for_string)
         return render_template('rooms.html', form=form)
     if request.method == 'GET':
+        room_list = Rooms.query.all()
+        room_dict = {}
+        for item in room_list:
+            item_dict = {"name": item.room_name}
+            room_dict[str(item.id)] = item_dict
         form = ChatForm()
         form.room.data = session.get('room', '')
-        return render_template('rooms.html', form=form)
+        return render_template('rooms.html', form=form, room_dict=room_dict)
